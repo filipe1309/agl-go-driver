3b2562505aa11552b7fe53c01b77bb9ed1eec6d0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-15 18:05:05 -0300

    feat: add folder struct

---
+++ internal/folders/entity.go
@@ -0,0 +1,41 @@
+package folders
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNameEmpty = errors.New("name is required")
+)
+
+func New(name string, parent_id int64) (*Folder, error) {
+	folder := Folder{
+		Name:      name,
+		ParentID:  parent_id,
+		UpdatedAt: time.Now(),
+	}
+	err := folder.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &folder, nil
+}
+
+type Folder struct {
+	ID        int64     `json:"id"`
+	ParentID  int64     `json:"parent_id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Deleted   bool      `json:"-"`
+}
+
+func (f *Folder) Validate() error {
+	if f.Name == "" {
+		return ErrNameEmpty
+	}
+
+	return nil
+}
