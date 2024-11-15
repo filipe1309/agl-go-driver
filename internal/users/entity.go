12d501b2c7d55bf8bd33c74117c04480c5dfe566
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-15 14:40:20 -0300

    feat: add user setPassword method

--- internal/users/entity.go
+++ internal/users/entity.go
@@ -1,6 +1,16 @@
 package users
 
-import "time"
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"time"
+)
+
+var (
+	ErrPasswordEmpty = errors.New("password can't be empty")
+	ErrPasswordTooShort = errors.New("password is too short, minimum 6 characters")
+)
 
 func New(name, login, password string) (*User, error) {
 	now := time.Now()
@@ -9,6 +19,10 @@ func New(name, login, password string) (*User, error) {
 		Login:     login,
 		CreatedAt: now,
 		UpdatedAt: now,
+	}
+	err := user.SetPassword(password)
+	if err != nil {
+		return nil, err
 	}
 
 	return &user, nil
@@ -23,4 +37,18 @@ type User struct {
 	UpdatedAt time.Time
 	LastLogin time.Time
 	Deleted   bool
+}
+
+func (u *User) SetPassword(password string) error {
+	if password == "" {
+		return ErrPasswordEmpty
+	}
+
+	if len(password) < 6 {
+		return ErrPasswordTooShort
+	}
+
+	u.Password = fmt.Sprintf("%x", md5.Sum([]byte(password)))
+
+	return nil
 }
