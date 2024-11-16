205be5ce0500dc9e2e507e652b2c42a059225fe8
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-15 21:12:38 -0300

    feat: add file upload logic

--- internal/files/create.go
+++ internal/files/create.go
@@ -3,33 +3,79 @@ package files
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/filipe1309/agl-go-driver/internal/queue"
 )
 
 func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
-	file := new(File)
-	err := json.NewDecoder(r.Body).Decode(file)
+	r.ParseMultipartForm(32 << 20) // 32MB
+
+	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	defer file.Close()
 
-	err = file.Validate()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	path := fmt.Sprintf("/%s", fileHeader.Filename)
 
-	id, err := Insert(h.db, file)
+	err = h.bucket.Upload(file, path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	file.ID = id
+	ownerID := 1
+	entity, err := New(int64(ownerID), fileHeader.Filename, fileHeader.Header.Get("Content-Type"), path)
+	if err != nil {
+		h.bucket.Delete(path)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	folderID := r.Form.Get("folder_id")
+	if folderID != "" {
+		folderIDInt, err := strconv.Atoi(folderID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		entity.FolderID = int64(folderIDInt)
+	}
+
+	id, err := Insert(h.db, entity)
+	if err != nil {
+		// h.bucket.Delete(path)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	entity.ID = id
+
+	dto := queue.QueueDTO{
+		Filename: fileHeader.Filename,
+		Path:     path,
+		ID:       int(id),
+	}
+
+	msg, err := dto.Marshal()
+	if err != nil {
+		// TODO: rollback
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = h.queue.Publish(msg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(file)
+	json.NewEncoder(w).Encode(entity)
 }
 
 func Insert(db *sql.DB, file *File) (int64, error) {
--- internal/files/routes.go
+++ internal/files/routes.go
@@ -3,15 +3,19 @@ package files
 import (
 	"database/sql"
 
+	"github.com/filipe1309/agl-go-driver/internal/bucket"
+	"github.com/filipe1309/agl-go-driver/internal/queue"
 	"github.com/go-chi/chi/v5"
 )
 
 type handler struct {
-	db *sql.DB
+	db     *sql.DB
+	bucket *bucket.Bucket
+	queue  *queue.Queue
 }
 
-func SetRoutes(router chi.Router, db *sql.DB) {
-	h := handler{db: db}
+func SetRoutes(router chi.Router, db *sql.DB, b *bucket.Bucket, q *queue.Queue) {
+	h := handler{db, b, q}
 
 	// router.Get("/folders", h.List)
 	router.Post("/folders", h.Create)
