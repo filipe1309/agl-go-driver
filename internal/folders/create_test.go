ba2c4e1b3b206843a3d002e8a35a93d1c6dc45df
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-18 18:57:28 -0300

    test: add unit test for Insert function in folders package

---
+++ internal/folders/create_test.go
@@ -0,0 +1,34 @@
+package folders
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestInsert(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	folder, err := New("Test name", 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (name, parent_id, updated_at) VALUES ($1, $2, $3)`)).
+		WithArgs("Test name", 0, sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err = Insert(db, folder)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
