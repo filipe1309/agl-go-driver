300ecf8f9ed0461498ca29dc054d46a52ca9af6a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-18 19:06:10 -0300

    test: add unit test for Read function in files package

---
+++ internal/files/get_test.go
@@ -0,0 +1,32 @@
+package files
+
+import (
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestRead(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
+		AddRow(1, 1, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false)
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE id = $1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	_, err = Read(db, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
