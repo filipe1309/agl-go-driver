7e994bc785d21d574d49a96ea6a1ac6bab38473e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 12:38:33 -0300

    test: add unit test for GetByID API handler

--- internal/users/get_test.go
+++ internal/users/get_test.go
@@ -1,12 +1,59 @@
 package users
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi/v5"
 )
+
+func TestGetByID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	user := &User{
+		ID:   1,
+		Name: "Test user 1",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(user)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/users/{id}", nil)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "updated_at", "last_login", "deleted"}).
+		AddRow(1, "Test name", "testlogin", "testpassword", time.Now(), time.Now(), time.Now(), false)
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM users WHERE id = $1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	h.GetByID(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+		fmt.Println(rr.Body.String())
+	}
+}
 
 func TestRead(t *testing.T) {
 	db, mock, err := sqlmock.New()
