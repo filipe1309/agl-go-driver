b74d6437bfa3369ef533773155b0af1c9e78d709
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 17:49:03 -0300

    fix: retrieve folder by ID and handle potential errors in Update handler

--- internal/folders/update.go
+++ internal/folders/update.go
@@ -36,7 +36,11 @@ func (h *handler) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: Get folder by ID and return it
+	folder, err = ReadDB(h.db, int64(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(folder)
