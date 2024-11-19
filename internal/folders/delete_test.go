2657a4bc260afeea6626658b71321a4596dee6ec
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 17:34:49 -0300

    test: enhance TestSoftDelete with additional file update expectations and mock rows

--- internal/folders/delete_test.go
+++ internal/folders/delete_test.go
@@ -28,12 +28,24 @@ func TestSoftDelete(t *testing.T) {
 	ctx.URLParams.Add("id", "1")
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
+	// files.ReadAllDB
 	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
-		AddRow(1, 1, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false).
-		AddRow(2, 1, 1, "Test name 2", "testtype", "testpath", time.Now(), time.Now(), true)
+		AddRow(1, 1, 1, "Test file 1", "testtype", "testpath", time.Now(), time.Now(), false).
+		AddRow(2, 1, 1, "Test file 2", "testtype", "testpath", time.Now(), time.Now(), false)
 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id = $1 AND deleted = FALSE`)).
 		WillReturnRows(rows)
 
+	// files.UpdateDB
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
+		WithArgs("Test file 1", sqlmock.AnyArg(), true, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// files.UpdateDB
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
+		WithArgs("Test file 2", sqlmock.AnyArg(), true, 2).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// readSubFolderDB
 	rows2 := sqlmock.NewRows([]string{"id", "parent_id", "name", "created_at", "updated_at", "deleted"}).
 		AddRow(2, 3, "Test sub name", time.Now(), time.Now(), false).
 		AddRow(3, 3, "Test sub name 2", time.Now(), time.Now(), false)
@@ -41,6 +53,7 @@ func TestSoftDelete(t *testing.T) {
 		WithArgs(1).
 		WillReturnRows(rows2)
 
+	// SoftDeleteDB
 	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET updated_at = $1, deleted = TRUE WHERE id = $2`)).
 		WithArgs(sqlmock.AnyArg(), 1).
 		WillReturnResult(sqlmock.NewResult(1, 1))
