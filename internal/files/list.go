4d1eabdd940db077b596173f43c5c4e725546b78
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-18 22:28:23 -0300

    refactor: extract file selection logic into a separate function for improved readability

--- internal/files/list.go
+++ internal/files/list.go
@@ -7,31 +7,15 @@ import (
 
 func ReadAll(db *sql.DB, folder_id int64) ([]File, error) {
 	stmt := `SELECT * FROM files WHERE folder_id = $1 AND deleted = FALSE`
-
-	rows, err := db.Query(stmt, folder_id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	files := make([]File, 0)
-	for rows.Next() {
-		var file File
-		err := rows.Scan(&file.ID, &file.FolderID, &file.OwnerID, &file.Name, &file.Type, &file.Path, &file.CreatedAt, &file.UpdatedAt, &file.Deleted)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-
-		files = append(files, file)
-	}
-
-	return files, nil
+	return selectAllFiles(db, stmt)
 }
 
 func ReadAllRoot(db *sql.DB) ([]File, error) {
 	stmt := `SELECT * FROM files WHERE folder_id IS NULL AND deleted = FALSE`
+	return selectAllFiles(db, stmt)
+}
 
+func selectAllFiles(db *sql.DB, stmt string) ([]File, error) {
 	rows, err := db.Query(stmt)
 	if err != nil {
 		return nil, err
