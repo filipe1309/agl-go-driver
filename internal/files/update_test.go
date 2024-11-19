7423518df823b94ed3fba29e3fc3f42b9b95f661
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 17:13:37 -0300

    test: add unit test for Update function in files package

--- internal/files/update_test.go
+++ internal/files/update_test.go
@@ -1,11 +1,65 @@
 package files
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi/v5"
 )
+
+func TestUpdate(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db, nil, nil}
+
+	file := &File{
+		ID:   1,
+		Name: "Test file 2",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/files/{id}", &b)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	// ReadDB
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
+		AddRow(1, 1, 3, "Test file 1", "testtype", "testpath", time.Now(), time.Now(), false)
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE id = $1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	// UpdateDB
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
+		WithArgs("Test file 2", sqlmock.AnyArg(), false, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.Update(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+		fmt.Println(rr.Body.String())
+	}
+}
 
 func TestUpdateDB(t *testing.T) {
 	db, mock, err := sqlmock.New()
