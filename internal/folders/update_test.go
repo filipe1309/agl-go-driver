5c5099158c0e1e80c56f886a1906fe3114ca3c61
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 15:37:45 -0300

    test: add unit test for UpdateAPI function in folders package

--- internal/folders/update_test.go
+++ internal/folders/update_test.go
@@ -1,11 +1,56 @@
 package folders
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi/v5"
 )
+
+func TestUpdateAPI(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	user := &Folder{
+		ID:   1,
+		Name: "Test folder 1",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(user)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/folders/{id}", &b)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET name = $1, updated_at = $2 WHERE id = $3`)).
+		WithArgs("Test folder 1", sqlmock.AnyArg(), 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.Update(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+		fmt.Println(rr.Body.String())
+	}
+}
 
 func TestUpdate(t *testing.T) {
 	db, mock, err := sqlmock.New()
