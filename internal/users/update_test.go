b8f1ccf4a361040efebeff5cfd8c13fe8623e32b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 12:29:49 -0300

    test: add unit test for Update API handler

--- internal/users/update_test.go
+++ internal/users/update_test.go
@@ -1,11 +1,63 @@
 package users
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi/v5"
 )
+
+func TestUpdateAPI(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	user := &User{
+		ID:   1,
+		Name: "Test user 1",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(user)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/users/{id}", &b)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE users SET name = $1, updated_at = $3 WHERE id = $4`)).
+		WithArgs("Test user 1", sqlmock.AnyArg(), 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "updated_at", "last_login", "deleted"}).
+		AddRow(1, "Test name", "testlogin", "testpassword", time.Now(), time.Now(), time.Now(), false)
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM users WHERE id = $1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	h.Update(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+		fmt.Println(rr.Body.String())
+	}
+}
 
 func TestUpdate(t *testing.T) {
 	db, mock, err := sqlmock.New()
