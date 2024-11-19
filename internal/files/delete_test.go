8ad2b1472fd95cd7373fe981eca7e7d114435560
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 17:21:51 -0300

    test: add unit test for SoftDelete function in files package

--- internal/files/delete_test.go
+++ internal/files/delete_test.go
@@ -1,11 +1,43 @@
 package files
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi/v5"
 )
+
+func TestSoftDelete(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db, nil, nil}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodDelete, "/folders/{id}", nil)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET updated_at = $1, deleted = TRUE WHERE id = $2`)).
+		WithArgs(sqlmock.AnyArg(), 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.SoftDelete(rr, req)
+
+	if rr.Code != http.StatusNoContent {
+		t.Errorf("Expected status code %d, got %d", http.StatusNoContent, rr.Code)
+		fmt.Println(rr.Body.String())
+	}
+}
 
 func TestSoftDeleteDB(t *testing.T) {
 	db, mock, err := sqlmock.New()
