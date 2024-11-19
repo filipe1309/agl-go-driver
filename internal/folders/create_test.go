d25a7417f62844d84ae2667eeafd247dc192c465
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 14:17:17 -0300

    test: add unit tests for Create function and handle folder insertion with and without parent

--- internal/folders/create_test.go
+++ internal/folders/create_test.go
@@ -1,13 +1,53 @@
 package folders
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
-func TestInsert(t *testing.T) {
+func TestCreate(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	folder, err := New("Test name", 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(folder)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/folders", &b)
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
+		WithArgs(nil, "Test name", sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.Create(rr, req)
+
+	if rr.Code != http.StatusCreated {
+		t.Errorf("Expected status code %d, got %d", http.StatusCreated, rr.Code)
+		fmt.Println(rr.Body.String())
+	}
+}
+
+func TestInsertRoot(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Error(err)
@@ -21,6 +61,32 @@ func TestInsert(t *testing.T) {
 
 	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
 		WithArgs(nil, "Test name", sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err = Insert(db, folder)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInsertWithFolder(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	folder, err := New("Test name", 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
+		WithArgs(1, "Test name", sqlmock.AnyArg()).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
 	_, err = Insert(db, folder)
