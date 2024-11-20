127313b115a2bbccb547ba685df5d46508c57e00
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 12:56:03 -0300

    refactor: migrate TestReadAllDB and TestReadAllRootDB to FileTransactionSuite for improved organization and assertions

--- internal/files/list_test.go
+++ internal/files/list_test.go
@@ -2,58 +2,50 @@ package files
 
 import (
 	"regexp"
-	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestReadAllDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *FileTransactionSuite) TestReadAllDB() {
+	// Arrange
+	setMockReadAllDB(ts.mock)
 
+	// Act
+	_, err := ReadAllDB(ts.conn, 0)
+
+	// Assert
+	assert.NoError(ts.T(), err)
+
+	// if len(list) > 1 { // this doesn't work with sqlmock, because it doesn't filter the rows
+	// 	t.Error("Expected 1 row, got", len(list))
+	// }
+}
+
+func (ts *FileTransactionSuite) TestReadAllRootDB() {
+	// Arrange
+	setMockReadAllRootDB(ts.mock)
+
+	// Act
+	_, err := ReadAllRootDB(ts.conn)
+
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockReadAllRootDB(mock sqlmock.Sqlmock) {
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
+		AddRow(1, nil, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false).
+		AddRow(2, nil, 1, "Test name 2", "testtype", "testpath", time.Now(), time.Now(), true)
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id IS NULL AND deleted = FALSE`)).
+		WillReturnRows(rows)
+}
+
+func setMockReadAllDB(mock sqlmock.Sqlmock) {
 	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
 		AddRow(1, 1, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false).
 		AddRow(2, 1, 1, "Test name 2", "testtype", "testpath", time.Now(), time.Now(), true)
 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id = $1 AND deleted = FALSE`)).
 		WillReturnRows(rows)
-
-	_, err = ReadAllDB(db, 0)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// if len(list) > 1 { // this doesn't work with sqlmock, because it doesn't filter the rows
-	// 	t.Error("Expected 1 row, got", len(list))
-	// }
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
-}
-
-func TestReadAllRootDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
-		AddRow(1, 1, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false).
-		AddRow(2, 1, 1, "Test name 2", "testtype", "testpath", time.Now(), time.Now(), true)
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id IS NULL AND deleted = FALSE`)).
-		WillReturnRows(rows)
-
-	_, err = ReadAllRootDB(db)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
 }
