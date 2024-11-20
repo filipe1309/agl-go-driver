7a8a5f9285a3c93065ddf5ccee4d6fa884c68388
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-19 23:55:15 -0300

    feat: refactor GetByID and ReadDB tests to use TransactionSuite for improved organization and assertions

--- internal/users/get_test.go
+++ internal/users/get_test.go
@@ -2,65 +2,48 @@ package users
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
-	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetByID(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	h := handler{db}
-
+func (ts *TransactionSuite) TestGetByID() {
+	// Arrange
 	rr := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodGet, "/users/{id}", nil)
 	ctx := chi.NewRouteContext()
 	ctx.URLParams.Add("id", "1")
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
-	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "updated_at", "last_login", "deleted"}).
-		AddRow(1, "Test name", "testlogin", "testpassword", time.Now(), time.Now(), time.Now(), false)
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM users WHERE id = $1`)).
-		WithArgs(1).
-		WillReturnRows(rows)
+	setMockReadDB(ts.mock)
 
-	h.GetByID(rr, req)
+	// Act
+	ts.handler.GetByID(rr, req)
 
-	if rr.Code != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
-		fmt.Println(rr.Body.String())
-	}
+	// Assert
+	assert.Equal(ts.T(), http.StatusOK, rr.Code)
 }
 
-func TestReadDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *TransactionSuite) TestReadDB() {
+	// Arrange
+	setMockReadDB(ts.mock)
 
+	// Act
+	_, err := ReadDB(ts.conn, 1)
+
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockReadDB(mock sqlmock.Sqlmock) {
 	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "updated_at", "last_login", "deleted"}).
 		AddRow(1, "Test name", "testlogin", "testpassword", time.Now(), time.Now(), time.Now(), false)
 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM users WHERE id = $1`)).
 		WithArgs(1).
 		WillReturnRows(rows)
-
-	_, err = ReadDB(db, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
 }
