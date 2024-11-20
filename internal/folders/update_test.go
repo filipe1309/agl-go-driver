ba56e1acec24b7f9913fe2d4e2a2b896efcfdf18
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 12:24:21 -0300

    refactor: migrate Update and UpdateDB tests to FolderTransactionSuite for improved organization and assertions

--- internal/folders/update_test.go
+++ internal/folders/update_test.go
@@ -4,36 +4,25 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
-	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdate(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	h := handler{db}
-
+func (ts *FolderTransactionSuite) TestUpdate() {
+	// Arrange
 	folder := &Folder{
 		ID:   1,
 		Name: "Test folder 1",
 	}
 
 	var b bytes.Buffer
-	err = json.NewEncoder(&b).Encode(folder)
-	if err != nil {
-		t.Error(err)
-	}
+	err := json.NewEncoder(&b).Encode(folder)
+	assert.NoError(ts.T(), err)
 
 	rr := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodPut, "/folders/{id}", &b)
@@ -41,45 +30,31 @@ func TestUpdate(t *testing.T) {
 	ctx.URLParams.Add("id", "1")
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
-	// UpdateDB
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET name = $1, updated_at = $2 WHERE id = $3`)).
-		WithArgs("Test folder 1", sqlmock.AnyArg(), 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+	setMockUpdateDB(ts.mock, 1)
+	setMockReadFolderDB(ts.mock)
 
-	// ReadFolderDB
-	rows := sqlmock.NewRows([]string{"id", "parent_id", "name", "created_at", "updated_at", "deleted"}).
-		AddRow(1, 2, "Test folder 1", time.Now(), time.Now(), false)
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM folders WHERE id = $1`)).
-		WithArgs(1).
-		WillReturnRows(rows)
+	// Act
+	ts.handler.Update(rr, req)
 
-	h.Update(rr, req)
-
-	if rr.Code != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
-		fmt.Println(rr.Body.String())
-	}
+	// Assert
+	assert.Equal(ts.T(), http.StatusOK, rr.Code)
 }
 
-func TestUpdateDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *FolderTransactionSuite) TestUpdateDB() {
+	// Arrange
+	setMockUpdateDB(ts.mock, 1)
 
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET name = $1, updated_at = $2 WHERE id = $3`)).
-		WithArgs("Test name", sqlmock.AnyArg(), 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	_, err = UpdateDB(db, 1, &Folder{
-		Name: "Test name",
+	// Act
+	_, err := UpdateDB(ts.conn, 1, &Folder{
+		Name: "Test folder 1",
 	})
-	if err != nil {
-		t.Error(err)
-	}
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockUpdateDB(mock sqlmock.Sqlmock, id int) {
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET name = $1, updated_at = $2 WHERE id = $3`)).
+		WithArgs("Test folder 1", sqlmock.AnyArg(), id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
 }
