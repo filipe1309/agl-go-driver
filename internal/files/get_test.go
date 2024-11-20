e135b2fc5b0304f1757bdf53e4c2d50c63f7c3f0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 12:44:51 -0300

    refactor: migrate TestReadDB to FileTransactionSuite for improved organization and assertions

--- internal/files/get_test.go
+++ internal/files/get_test.go
@@ -2,31 +2,27 @@ package files
 
 import (
 	"regexp"
-	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestReadDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *FileTransactionSuite) TestReadDB() {
+	// Arrange
+	setMockReadDB(ts.mock)
 
+	// Act
+	_, err := ReadDB(ts.conn, 1)
+
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockReadDB(mock sqlmock.Sqlmock) {
 	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
 		AddRow(1, 1, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false)
 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE id = $1`)).
 		WithArgs(1).
 		WillReturnRows(rows)
-
-	_, err = ReadDB(db, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
 }
