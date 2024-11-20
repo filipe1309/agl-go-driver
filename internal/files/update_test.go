aab90708097d9b191d417325698d928751f1538a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 13:04:04 -0300

    refactor: migrate TestUpdate and TestUpdateDB to FileTransactionSuite for improved organization and assertions

--- internal/files/update_test.go
+++ internal/files/update_test.go
@@ -4,36 +4,25 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
-	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdate(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	h := handler{db, nil, nil}
-
+func (ts *FileTransactionSuite) TestUpdate() {
+	// Arrange
 	file := &File{
 		ID:   1,
 		Name: "Test file 2",
 	}
 
 	var b bytes.Buffer
-	err = json.NewEncoder(&b).Encode(file)
-	if err != nil {
-		t.Error(err)
-	}
+	err := json.NewEncoder(&b).Encode(file)
+	assert.NoError(ts.T(), err)
 
 	rr := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodPut, "/files/{id}", &b)
@@ -41,45 +30,31 @@ func TestUpdate(t *testing.T) {
 	ctx.URLParams.Add("id", "1")
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
-	// ReadDB
-	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
-		AddRow(1, 1, 3, "Test file 1", "testtype", "testpath", time.Now(), time.Now(), false)
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE id = $1`)).
-		WithArgs(1).
-		WillReturnRows(rows)
+	setMockReadDB(ts.mock)
+	setMockUpdateDB(ts.mock, 1)
 
-	// UpdateDB
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
-		WithArgs("Test file 2", sqlmock.AnyArg(), false, 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+	// Act
+	ts.handler.Update(rr, req)
 
-	h.Update(rr, req)
-
-	if rr.Code != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
-		fmt.Println(rr.Body.String())
-	}
+	// Assert
+	assert.Equal(ts.T(), http.StatusOK, rr.Code)
 }
 
-func TestUpdateDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *FileTransactionSuite) TestUpdateDB() {
+	// Arrange
+	setMockUpdateDB(ts.mock, 1)
 
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
-		WithArgs("Test name", sqlmock.AnyArg(), false, 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	_, err = UpdateDB(db, 1, &File{
-		Name: "Test name",
+	// Act
+	_, err := UpdateDB(ts.conn, 1, &File{
+		Name: "Test file 2",
 	})
-	if err != nil {
-		t.Error(err)
-	}
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockUpdateDB(mock sqlmock.Sqlmock, id int) {
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
+		WithArgs("Test file 2", sqlmock.AnyArg(), false, id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
 }
