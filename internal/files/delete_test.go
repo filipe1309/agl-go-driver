70af5fb6f8222335efd228ff3c1370506608f111
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 12:42:12 -0300

    refactor: migrate SoftDelete and SoftDeleteDB tests to FileTransactionSuite for improved organization and assertions

--- internal/files/delete_test.go
+++ internal/files/delete_test.go
@@ -2,61 +2,46 @@ package files
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
-	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSoftDelete(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	h := handler{db, nil, nil}
-
+func (ts *FileTransactionSuite) TestSoftDelete() {
+	// Arrange
 	rr := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodDelete, "/folders/{id}", nil)
 	ctx := chi.NewRouteContext()
 	ctx.URLParams.Add("id", "1")
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET updated_at = $1, deleted = TRUE WHERE id = $2`)).
-		WithArgs(sqlmock.AnyArg(), 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+	setMockSoftDeleteDB(ts.mock)
 
-	h.SoftDelete(rr, req)
+	// Act
+	ts.handler.SoftDelete(rr, req)
 
-	if rr.Code != http.StatusNoContent {
-		t.Errorf("Expected status code %d, got %d", http.StatusNoContent, rr.Code)
-		fmt.Println(rr.Body.String())
-	}
+	// Assert
+	assert.Equal(ts.T(), http.StatusNoContent, rr.Code)
 }
 
-func TestSoftDeleteDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *FileTransactionSuite) TestSoftDeleteDB() {
+	// Arrange
+	setMockSoftDeleteDB(ts.mock)
 
+	// Act
+	err := SoftDeleteDB(ts.conn, 1)
+
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockSoftDeleteDB(mock sqlmock.Sqlmock) {
 	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET updated_at = $1, deleted = TRUE WHERE id = $2`)).
 		// WithArgs(AnyTime{}, 1).
 		WithArgs(sqlmock.AnyArg(), 1).
 		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	err = SoftDeleteDB(db, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
 }
