4ea10b849e3365e1bc485dd3310f9b4ec1509f64
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 00:27:09 -0300

    feat: refactor folder creation tests to use FolderTransactionSuite for improved organization and assertions

--- internal/folders/create_test.go
+++ internal/folders/create_test.go
@@ -3,98 +3,59 @@ package folders
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
-	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/filipe1309/agl-go-driver/internal/common"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCreate(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	h := handler{db}
-
-	folder, err := New("Test name", 0)
-	if err != nil {
-		t.Error(err)
-	}
-
+func (ts *FolderTransactionSuite) TestCreate() {
+	// Arrange
 	var b bytes.Buffer
-	err = json.NewEncoder(&b).Encode(folder)
-	if err != nil {
-		t.Error(err)
-	}
+	err := json.NewEncoder(&b).Encode(ts.entity)
+	assert.NoError(ts.T(), err)
 
 	rr := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodPost, "/folders", &b)
 
-	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
-		WithArgs(nil, "Test name", sqlmock.AnyArg()).
+	ts.mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
+		WithArgs(nil, "folder1", sqlmock.AnyArg()).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	h.Create(rr, req)
+	// Act
+	ts.handler.Create(rr, req)
 
-	if rr.Code != http.StatusCreated {
-		t.Errorf("Expected status code %d, got %d", http.StatusCreated, rr.Code)
-		fmt.Println(rr.Body.String())
-	}
+	// Assert
+	assert.Equal(ts.T(), http.StatusCreated, rr.Code)
 }
 
-func TestInsertRootDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	folder, err := New("Test name", 0)
-	if err != nil {
-		t.Error(err)
-	}
-
-	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
-		WithArgs(nil, "Test name", sqlmock.AnyArg()).
+func (ts *FolderTransactionSuite) TestInsertRootDB() {
+	// Arrange
+	ts.mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
+		WithArgs(nil, "folder1", sqlmock.AnyArg()).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	_, err = InsertDB(db, folder)
-	if err != nil {
-		t.Error(err)
-	}
+	// Act
+	_, err := InsertDB(ts.conn, ts.entity)
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
+	// Assert
+	assert.NoError(ts.T(), err)
 }
 
-func TestInsertWithFolderDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *FolderTransactionSuite) TestInsertWithFolderDB() {
+	// Arrange
+	ts.entity.ParentID = common.ValidNullInt64(1)
 
-	folder, err := New("Test name", 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
-		WithArgs(1, "Test name", sqlmock.AnyArg()).
+	ts.mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO folders (parent_id, name, updated_at) VALUES ($1, $2, $3)`)).
+		WithArgs(1, "folder1", sqlmock.AnyArg()).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	_, err = InsertDB(db, folder)
-	if err != nil {
-		t.Error(err)
-	}
+	// Act
+	_, err := InsertDB(ts.conn, ts.entity)
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
+	// Assert
+	assert.NoError(ts.T(), err)
 }
