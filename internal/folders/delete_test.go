f537ec07fde0426381549cd68bdf9dedccce28dc
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-20 00:53:45 -0300

    refactor: delete tests to use FolderTransactionSuite for improved organization and assertions

--- internal/folders/delete_test.go
+++ internal/folders/delete_test.go
@@ -2,88 +2,72 @@ package folders
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
-	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSoftDelete(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
-	h := handler{db}
-
+func (ts *FolderTransactionSuite) TestSoftDelete() {
+	// Arrange
 	rr := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodDelete, "/folders/{id}", nil)
 	ctx := chi.NewRouteContext()
 	ctx.URLParams.Add("id", "1")
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
-	// files.ReadAllDB
+	setMockFilesReadAllDB(ts.mock)
+	setMockFilesUpdateDB(ts.mock, "Test file 1", 1)
+	setMockFilesUpdateDB(ts.mock, "Test file 2", 2)
+	setMockReadSubFolderDB(ts.mock)
+	setSoftDeleteDB(ts.mock)
+
+	ts.handler.SoftDelete(rr, req)
+
+	assert.Equal(ts.T(), http.StatusNoContent, rr.Code)
+}
+
+func (ts *FolderTransactionSuite) TestSoftDeleteDB() {
+	// Arrange
+	setSoftDeleteDB(ts.mock)
+
+	// Act
+	err := SoftDeleteDB(ts.conn, 1)
+
+	// Assert
+	assert.NoError(ts.T(), err)
+}
+
+func setMockFilesReadAllDB(mock sqlmock.Sqlmock) {
 	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
 		AddRow(1, 1, 1, "Test file 1", "testtype", "testpath", time.Now(), time.Now(), false).
 		AddRow(2, 1, 1, "Test file 2", "testtype", "testpath", time.Now(), time.Now(), false)
 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id = $1 AND deleted = FALSE`)).
 		WillReturnRows(rows)
+}
 
-	// files.UpdateDB
+func setMockFilesUpdateDB(mock sqlmock.Sqlmock, fileName string, id int) {
 	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
-		WithArgs("Test file 1", sqlmock.AnyArg(), true, 1).
+		WithArgs(fileName, sqlmock.AnyArg(), true, id).
 		WillReturnResult(sqlmock.NewResult(1, 1))
+}
 
-	// files.UpdateDB
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE files SET name = $1, updated_at = $2, deleted = $3 WHERE id = $4`)).
-		WithArgs("Test file 2", sqlmock.AnyArg(), true, 2).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	// readSubFolderDB
-	rows2 := sqlmock.NewRows([]string{"id", "parent_id", "name", "created_at", "updated_at", "deleted"}).
+func setMockReadSubFolderDB(mock sqlmock.Sqlmock) {
+	rows := sqlmock.NewRows([]string{"id", "parent_id", "name", "created_at", "updated_at", "deleted"}).
 		AddRow(2, 3, "Test sub name", time.Now(), time.Now(), false).
 		AddRow(3, 3, "Test sub name 2", time.Now(), time.Now(), false)
 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM folders WHERE parent_id = $1 AND deleted = FALSE`)).
 		WithArgs(1).
-		WillReturnRows(rows2)
-
-	// SoftDeleteDB
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET updated_at = $1, deleted = TRUE WHERE id = $2`)).
-		WithArgs(sqlmock.AnyArg(), 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	h.SoftDelete(rr, req)
-
-	if rr.Code != http.StatusNoContent {
-		t.Errorf("Expected status code %d, got %d", http.StatusNoContent, rr.Code)
-		fmt.Println(rr.Body.String())
-	}
+		WillReturnRows(rows)
 }
 
-func TestSoftDeleteDB(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-
+func setSoftDeleteDB(mock sqlmock.Sqlmock) {
 	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET updated_at = $1, deleted = TRUE WHERE id = $2`)).
 		// WithArgs(AnyTime{}, 1).
 		WithArgs(sqlmock.AnyArg(), 1).
 		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	err = SoftDeleteDB(db, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
 }
