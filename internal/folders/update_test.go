33c89ed971fe5f79a7bafe67d4d56dd80b4a3284
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-21 16:47:35 -0300

    test: enhance Update function tests with comprehensive scenarios and error handling

--- internal/folders/update_test.go
+++ internal/folders/update_test.go
@@ -3,7 +3,9 @@ package folders
 import (
 	"bytes"
 	"context"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
@@ -14,35 +16,114 @@ import (
 )
 
 func (ts *FolderTransactionSuite) TestUpdate() {
-	// Arrange
-	folder := &Folder{
-		ID:   1,
-		Name: "Test folder 1",
+	tcs := []struct {
+		Name                    string
+		ID                      int
+		IDStr                   string
+		WithMock                bool
+		MockFolder              *Folder
+		MockUpdatedDBWithErr    bool
+		MockReadFolderDBWithErr bool
+		ExpectedStatusCode      int
+	}{
+		{
+			Name:                    "Success",
+			ID:                      1,
+			IDStr:                   "1",
+			WithMock:                true,
+			MockFolder:              &Folder{ID: 1, Name: "Test folder 1"},
+			MockUpdatedDBWithErr:    false,
+			MockReadFolderDBWithErr: false,
+			ExpectedStatusCode:      http.StatusOK,
+		},
+		{
+			Name:                    "Invalid folder - no name",
+			ID:                      2,
+			IDStr:                   "2",
+			WithMock:                false,
+			MockFolder:              &Folder{ID: 2},
+			MockUpdatedDBWithErr:    false,
+			MockReadFolderDBWithErr: false,
+			ExpectedStatusCode:      http.StatusBadRequest,
+		},
+		{
+			Name:                    "Invalid json - empty body",
+			ID:                      3,
+			IDStr:                   "3",
+			WithMock:                false,
+			MockFolder:              &Folder{},
+			MockUpdatedDBWithErr:    false,
+			MockReadFolderDBWithErr: false,
+			ExpectedStatusCode:      http.StatusBadRequest,
+		},
+		{
+			Name:                    "Invalid url param - id",
+			ID:                      -1,
+			IDStr:                   "A",
+			WithMock:                false,
+			MockFolder:              &Folder{ID: 1, Name: "Test folder 1"},
+			MockUpdatedDBWithErr:    false,
+			MockReadFolderDBWithErr: false,
+			ExpectedStatusCode:      http.StatusBadRequest,
+		},
+		{
+			Name:                    "DB error - update",
+			ID:                      1,
+			IDStr:                   "1",
+			WithMock:                true,
+			MockFolder:              &Folder{ID: 1, Name: "Test folder 1"},
+			MockUpdatedDBWithErr:    true,
+			MockReadFolderDBWithErr: false,
+			ExpectedStatusCode:      http.StatusInternalServerError,
+		},
+
+		{
+			Name:                    "DB error - no read id",
+			ID:                      1,
+			IDStr:                   "1",
+			WithMock:                true,
+			MockFolder:              &Folder{ID: 1, Name: "Test folder 1"},
+			MockUpdatedDBWithErr:    false,
+			MockReadFolderDBWithErr: true,
+			ExpectedStatusCode:      http.StatusInternalServerError,
+		},
 	}
 
-	var b bytes.Buffer
-	err := json.NewEncoder(&b).Encode(folder)
-	assert.NoError(ts.T(), err)
+	for _, tc := range tcs {
+		// Arrange
+		var b bytes.Buffer
 
-	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodPut, "/folders/{id}", &b)
-	ctx := chi.NewRouteContext()
-	ctx.URLParams.Add("id", "1")
-	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+		if tc.Name != "Invalid json - empty body" {
+			err := json.NewEncoder(&b).Encode(tc.MockFolder)
+			assert.NoError(ts.T(), err)
+		}
 
-	setMockUpdateDB(ts.mock, 1)
-	setMockReadFolderDB(ts.mock)
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPut, "/folders/{id}", &b)
+		ctx := chi.NewRouteContext()
+		ctx.URLParams.Add("id", tc.IDStr)
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
-	// Act
-	ts.handler.Update(rr, req)
+		fmt.Println(tc.Name)
+		if tc.WithMock {
+			fmt.Println("WithMock", tc.MockUpdatedDBWithErr)
+			setMockUpdateDB(ts.mock, tc.ID, tc.MockUpdatedDBWithErr)
+			if !tc.MockUpdatedDBWithErr {
+				setMockReadFolderDB(ts.mock, tc.MockReadFolderDBWithErr)
+			}
+		}
 
-	// Assert
-	assert.Equal(ts.T(), http.StatusOK, rr.Code)
+		// Act
+		ts.handler.Update(rr, req)
+
+		// Assert
+		assert.Equal(ts.T(), tc.ExpectedStatusCode, rr.Code)
+	}
 }
 
 func (ts *FolderTransactionSuite) TestUpdateDB() {
 	// Arrange
-	setMockUpdateDB(ts.mock, 1)
+	setMockUpdateDB(ts.mock, 1, false)
 
 	// Act
 	_, err := UpdateDB(ts.conn, 1, &Folder{
@@ -53,8 +134,13 @@ func (ts *FolderTransactionSuite) TestUpdateDB() {
 	assert.NoError(ts.T(), err)
 }
 
-func setMockUpdateDB(mock sqlmock.Sqlmock, id int) {
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET name = $1, updated_at = $2 WHERE id = $3`)).
-		WithArgs("Test folder 1", sqlmock.AnyArg(), id).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+func setMockUpdateDB(mock sqlmock.Sqlmock, id int, err bool) {
+	exp := mock.ExpectExec(regexp.QuoteMeta(`UPDATE folders SET name = $1, updated_at = $2 WHERE id = $3`)).
+		WithArgs("Test folder 1", sqlmock.AnyArg(), id)
+
+	if err {
+		exp.WillReturnError(sql.ErrNoRows)
+	} else {
+		exp.WillReturnResult(sqlmock.NewResult(1, 1))
+	}
 }
