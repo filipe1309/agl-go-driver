b25913577d4c60e5f889d7d928dc043562f8df2e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-22 18:02:39 -0300

    test: refactor TestList to use table-driven tests for improved coverage and error handling

--- internal/folders/list_test.go
+++ internal/folders/list_test.go
@@ -1,6 +1,7 @@
 package folders
 
 import (
+	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
@@ -11,23 +12,53 @@ import (
 )
 
 func (ts *FolderTransactionSuite) TestList() {
-	// Arrange
-	rr := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	tcs := []struct {
+		Name                          string
+		MockReadAllDBWithErr          bool
+		MockFilesReadAllRootDBWithErr bool
+		ExpectedStatusCode            int
+	}{
+		{
+			Name:                          "Success",
+			MockReadAllDBWithErr:          false,
+			MockFilesReadAllRootDBWithErr: false,
+			ExpectedStatusCode:            http.StatusOK,
+		},
+		{
+			Name:                          "DB error - read root sub folder",
+			MockReadAllDBWithErr:          true,
+			MockFilesReadAllRootDBWithErr: false,
+			ExpectedStatusCode:            http.StatusInternalServerError,
+		},
+		{
+			Name:                          "DB error - read files",
+			MockReadAllDBWithErr:          false,
+			MockFilesReadAllRootDBWithErr: true,
+			ExpectedStatusCode:            http.StatusInternalServerError,
+		},
+	}
 
-	setMockReadRootSubFolderDB(ts.mock)
-	setMockFilesReadAllRootDB(ts.mock)
+	for _, tc := range tcs {
+		// Arrange
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/users", nil)
 
-	// Act
-	ts.handler.List(rr, req)
+		setMockReadRootSubFolderDB(ts.mock, tc.MockReadAllDBWithErr)
+		if !tc.MockReadAllDBWithErr {
+			setMockFilesReadAllRootDB(ts.mock, tc.MockFilesReadAllRootDBWithErr)
+		}
 
-	// Assert
-	assert.Equal(ts.T(), http.StatusOK, rr.Code)
+		// Act
+		ts.handler.List(rr, req)
+
+		// Assert
+		assert.Equal(ts.T(), tc.ExpectedStatusCode, rr.Code)
+	}
 }
 
 func (ts *FolderTransactionSuite) TestReadRootSubFolderDB() {
 	// Arrange
-	setMockReadRootSubFolderDB(ts.mock)
+	setMockReadRootSubFolderDB(ts.mock, false)
 
 	// Act
 	_, err := readRootSubFolderDB(ts.conn)
@@ -36,18 +67,26 @@ func (ts *FolderTransactionSuite) TestReadRootSubFolderDB() {
 	assert.NoError(ts.T(), err)
 }
 
-func setMockReadRootSubFolderDB(mock sqlmock.Sqlmock) {
+func setMockReadRootSubFolderDB(mock sqlmock.Sqlmock, err bool) {
 	rows := sqlmock.NewRows([]string{"id", "parent_id", "name", "created_at", "updated_at", "deleted"}).
 		AddRow(1, nil, "Test folder 1", time.Now(), time.Now(), false).
 		AddRow(5, nil, "Test folder 1", time.Now(), time.Now(), false)
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM folders WHERE parent_id IS NULL AND deleted = FALSE`)).
+	exp := mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM folders WHERE parent_id IS NULL AND deleted = FALSE`)).
 		WillReturnRows(rows)
+
+	if err {
+		exp.WillReturnError(sql.ErrNoRows)
+	}
 }
 
-func setMockFilesReadAllRootDB(mock sqlmock.Sqlmock) {
+func setMockFilesReadAllRootDB(mock sqlmock.Sqlmock, err bool) {
 	filesRow := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "updated_at", "deleted"}).
 		AddRow(1, nil, 1, "Test name", "testtype", "testpath", time.Now(), time.Now(), false).
 		AddRow(2, nil, 1, "Test name 2", "testtype", "testpath", time.Now(), time.Now(), true)
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id IS NULL AND deleted = FALSE`)).
+	exp := mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM files WHERE folder_id IS NULL AND deleted = FALSE`)).
 		WillReturnRows(filesRow)
+
+	if err {
+		exp.WillReturnError(sql.ErrNoRows)
+	}
 }
