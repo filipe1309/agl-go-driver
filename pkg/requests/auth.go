869c838eb24dcc13c1849262fda767c8afdcbabe
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-24 23:15:01 -0300

    feat: implement authentication request handling and token caching

--- pkg/requests/auth.go
+++ pkg/requests/auth.go
@@ -1,5 +1,57 @@
 package requests
 
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func Auth(path, username, password string) error {
-	return nil
+	creds := Credentials{username, password}
+
+	var body bytes.Buffer
+	err := json.NewEncoder(&body).Encode(creds)
+	if err != nil {
+		return err
+	}
+
+	resp, err := doRequest("POST", path, &body, false)
+	if err != nil {
+		return err
+	}
+
+	return createTokenCache(resp.Body)
+}
+
+type cacheToken struct {
+	Token string `json:"token"`
+}
+
+func createTokenCache(body io.ReadCloser) error {
+	token, err := io.ReadAll(body)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(".cacheToken")
+	if err != nil {
+		return err
+	}
+
+	cache := cacheToken{string(token)}
+
+	data, err := json.Marshal(&cache)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(data)
+
+	return err
 }
