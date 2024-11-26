cef6c7c84cd4684711cabe284b3a0ed9ea86c9d8
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-26 13:35:23 -0300

    feat: implement CLI entry point and register command modules for authentication, file, folder, and user management

---
+++ cmd/cli/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	authCmd "github.com/filipe1309/agl-go-driver/internal/auth/cmd"
+	filesCmd "github.com/filipe1309/agl-go-driver/internal/files/cmd"
+	foldersCmd "github.com/filipe1309/agl-go-driver/internal/folders/cmd"
+	usersCmd "github.com/filipe1309/agl-go-driver/internal/users/cmd"
+
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{}
+
+func main() {
+	authCmd.Register(RootCmd)
+	filesCmd.Register(RootCmd)
+	foldersCmd.Register(RootCmd)
+	usersCmd.Register(RootCmd)
+
+	if err := RootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
