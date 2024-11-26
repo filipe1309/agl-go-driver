155699297a8e0136c1584b5066784b8bf668c027
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-11-26 14:00:02 -0300

    feat: add build script for API, CLI, and worker applications

---
+++ scripts/shell/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apps=("api" "cli" "worker")
+values=("api" "drive" "worker")
+
+for i in "${!apps[@]}"; do
+  echo "Building ${values[$i]}..."
+  go build -o   ${values[$i]} ./cmd/${apps[$i]}/main.go
+done
