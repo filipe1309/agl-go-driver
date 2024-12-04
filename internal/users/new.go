22bb2a6d3897ebcac951facaf03cb6424d3bca29
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-12-04 13:46:51 -0300

    feat: add New function for user creation with validation

--- internal/users/new.go
+++ internal/users/new.go
@@ -1,5 +1,23 @@
 package users
 
+func New(id int64, name, login, password string) (*User, error) {
+	u := &User{
+		ID:       id,
+		Name:     name,
+		Login:    login,
+		Password: password,
+	}
+
+	err := u.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	// encryptPassword(u)
+
+	return u, nil
+}
+
 func newLoginUser(login, password string) *User {
 	u := &User{
 		Login:    login,
