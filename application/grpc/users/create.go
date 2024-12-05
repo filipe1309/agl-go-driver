3f3db7fe321deb6a3a42cc1aa5fd703f60f01e7c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-12-05 13:54:36 -0300

    refactor: simplify UserService Create method and extract response conversion

--- application/grpc/users/create.go
+++ application/grpc/users/create.go
@@ -5,7 +5,6 @@ import (
 
 	domain "github.com/filipe1309/agl-go-driver/internal/users"
 	pb "github.com/filipe1309/agl-go-driver/proto/v1/users"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (s *UserService) Create(ctx context.Context, in *pb.UserRequest) (*pb.UserResponse, error) {
@@ -19,15 +18,7 @@ func (s *UserService) Create(ctx context.Context, in *pb.UserRequest) (*pb.UserR
 		return &pb.UserResponse{Error: err.Error()}, err
 	}
 
-	ur := &pb.UserResponse{
-		User: &pb.User{
-			Id:        id,
-			Name:      user.Name,
-			Login:     user.Login,
-			CreatedAt: timestamppb.New(user.CreatedAt),
-			UpdatedAt: timestamppb.New(user.UpdatedAt),
-		},
-	}
+	user.ID = id
 
-	return ur, nil
+	return convertToUserResponse(user), nil
 }
--- application/grpc/users/server.go
+++ application/grpc/users/server.go
@@ -1,11 +1,27 @@
 package users
 
 import (
+	"github.com/filipe1309/agl-go-driver/factories"
+	domain "github.com/filipe1309/agl-go-driver/internal/users"
 	pb "github.com/filipe1309/agl-go-driver/proto/v1/users"
 	"github.com/filipe1309/agl-go-driver/repositories"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
-	repo repositories.UserWriteRepository
+	repo    repositories.UserWriteRepository
+	factory factories.UserFactory
+}
+
+func convertToUserResponse(user *domain.User) *pb.UserResponse {
+	return &pb.UserResponse{
+		User: &pb.User{
+			Id:        user.ID,
+			Name:      user.Name,
+			Login:     user.Login,
+			CreatedAt: timestamppb.New(user.CreatedAt),
+			UpdatedAt: timestamppb.New(user.UpdatedAt),
+		},
+	}
 }
